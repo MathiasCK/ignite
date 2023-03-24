987202f726bcbfd27657a54caeb36d52d33a9754
mathiasck <mathias.ck2000@gmail.com>
2023-03-24 18:55:01 +0100

    Include api key in fetch requests

--- src/api.js
+++ src/api.js
@@ -30,21 +30,22 @@ const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
 const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
 
 // Popular Games
-const popular_games = `games?dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
-const upcoming_games = `games?dates=${currentDate},${nextYear}&ordering=-added&page_size=10`;
-const new_games = `games?dates=${lastYear},${currentDate}&ordering=-released&page_size=10`;
+const popular_games = `games?key=${process.env.API_KEY}&dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+const upcoming_games = `games?key=${process.env.API_KEY}&dates=${currentDate},${nextYear}&ordering=-added&page_size=10`;
+const new_games = `games?key=${process.env.API_KEY}&dates=${lastYear},${currentDate}&ordering=-released&page_size=10`;
 
 export const popularGamesURL = () => `${base_url}${popular_games}`;
 export const upcomingGamesURL = () => `${base_url}${upcoming_games}`;
 export const newGamesURL = () => `${base_url}${new_games}`;
 
 // Game Details
-export const gameDetailsURL = (game_id) => `${base_url}games/${game_id}`;
+export const gameDetailsURL = game_id =>
+  `${base_url}games/${game_id}?key=${process.env.API_KEY}`;
 
 // Game Screenshots
-export const gameScreenshotURL = (game_id) =>
-  `${base_url}games/${game_id}/screenshots`;
+export const gameScreenshotURL = game_id =>
+  `${base_url}games/${game_id}/screenshots?key=${process.env.API_KEY}`;
 
 // Searched Game
-export const searchedGameURL = (game_name) =>
-  `${base_url}games?search=${game_name}&page_size=9`;
+export const searchedGameURL = game_name =>
+  `${base_url}games?key=${process.env.API_KEY}&search=${game_name}&page_size=9`;
